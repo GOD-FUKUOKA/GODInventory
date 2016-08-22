99a04e740a55bb7618049275fadd958b388b80d5
qinghe <areq22@gmail.com>
2016-08-22 22:40:29 +0800

    input stock manuafacturer filters

--- GODInventoryWinForm/Controls/InputStock.cs
+++ GODInventoryWinForm/Controls/InputStock.cs
@@ -14,7 +14,7 @@ namespace GODInventoryWinForm.Controls
 {
     public partial class InputStock : Form
     {
-        private List<MockEntity> manufacturerList;
+        private List<t_manufacturers> manufacturerList;
         private BindingList<v_stockios> stockiosList;
         private List<t_genre> genreList;
         private List<t_warehouses> warehouseList;
@@ -39,15 +39,15 @@ namespace GODInventoryWinForm.Controls
             {                
                 genreList = ctx.t_genre.ToList();
                 warehouseList = ctx.t_warehouses.ToList();
+                manufacturerList = ctx.t_manufacturers.ToList();
             }
             this.genreComboBox.DisplayMember = "ジャンル名";
             this.genreComboBox.ValueMember = "idジャンル";
             this.genreComboBox.DataSource = genreList;
 
-
-            this.manufacturerList = ManufactureRespository.ToList();
+            //this.manufacturerList = ManufactureRespository.ToList();
             this.manufacturerComboBox.DisplayMember = "FullName";
-            this.manufacturerComboBox.ValueMember = "Id"; 
+            this.manufacturerComboBox.ValueMember = "Id";
             this.manufacturerComboBox.DataSource = manufacturerList;
 
 
@@ -115,7 +115,17 @@ namespace GODInventoryWinForm.Controls
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+            //this.bindingSource1.Filter = String.Format( "genreId={0}", this.genreComboBox.SelectedValue);
+
+            var filtered = manufacturerList.FindAll( s=> s.genreId == (int)this.genreComboBox.SelectedValue);
+            if (filtered.Count > 0)
+            {
+                this.manufacturerComboBox.DataSource = filtered;
+            }
+            else {
+                this.manufacturerComboBox.DataSource = manufacturerList;
+            
+            }
             BuildStockNO();
            
         }
