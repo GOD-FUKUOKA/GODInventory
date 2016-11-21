3a3c842f17a63a7ba7c49419675d4ed2f5baf2c5
qinghe <areq22@gmail.com>
2016-11-21 17:29:30 +0800

    add cancel confirm form

---
+++ GODInventoryWinForm/Controls/CancelConfirmForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace GODInventoryWinForm.Controls
+{
+    partial class CancelConfirmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CancelConfirmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 262);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CancelConfirmForm";
+            this.Text = "CancelConfirmForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
